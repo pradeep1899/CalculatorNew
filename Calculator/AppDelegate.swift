62105e53cfe04c80957d6154d6b6dbd44167c3bf
buddybuild <help@buddybuild.com>
2017-04-05 08:36:55 -0700

    Adding buddybuild SDK
    - Add BuddyBuildSDK.framework
    - Initialize in App Delegate

--- Calculator/AppDelegate.swift
+++ Calculator/AppDelegate.swift
@@ -10,6 +10,11 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        BuddyBuildSDK.setup()
+        return true
+    }
+
                             
     var window: UIWindow?
 
